f9fafc449f9068862a13cc647dd698782784b6bf
Ema-Mu <info@ema.mu>
2017-07-03 14:34:12 +0200

    Modified schema function

--- ml-customize.js
+++ ml-customize.js
@@ -141,12 +141,7 @@ else if ( (url.indexOf("category=location") !== -1) && (url.indexOf("category=st
 };
 */
 
-var getPathFromUrl = function(url) {
-  return url.split("?")[0];
-}
-
 var schema = function () {
-
 
 var titles = document.getElementsByClassName('fluid-thumbnail-grid-image-title');
 var prices = document.getElementsByClassName('fluid-thumbnail-grid-image-price');
@@ -177,7 +172,7 @@ el.text = JSON.stringify({
         "about": { "@type": "LocalBusiness",
         "priceRange":prices[0].innerHTML,
         "paymentAccepted": "PayPal",
-        "image":getPathFromUrl(images[0].getAttribute('src'));,
+        "image":images[0].getAttribute('src').split("?")[0],
         "name":titles[0].innerHTML
       }
 
