fc57ee30bf1c51c7cfcd76b3c531ca0bea7099dd
Ema-Mu <info@ema.mu>
2017-09-02 21:45:52 +0200

    unminifying

--- ml-customize.js
+++ ml-customize.js
@@ -1,1 +1,321 @@
-function myFunction(t){var e=document.getElementById(t);"none"===e.style.display?e.style.display="block":e.style.display="none"}var set_social_icons=function(){var t=document.querySelectorAll(".MenuPriority>div:last-child");window.innerWidth>=996?t[0].insertAdjacentHTML("afterend",'<a href="https://www.facebook.com/misterlocationOfficial" target="blank"><i class="fa fa-facebook fa-2x"></i></a><a href="https://twitter.com/Mister_Location" target="blank"><i class="fa fa-twitter fa-2x"></i></a><a href="https://www.instagram.com/misterlocation/" target="blank"><i class="fa fa-instagram fa-2x"></i></a>'):document.querySelectorAll(".MenuSection>div:last-child")[0].insertAdjacentHTML("afterend",'<div class="mobilesocial"><a href="https://www.facebook.com/misterlocationOfficial" target="blank"><i class="fa fa-facebook fa-2x"></i></a><a href="https://twitter.com/Mister_Location" target="blank"><i class="fa fa-twitter fa-2x"></i></a><a href="https://www.instagram.com/misterlocation/" target="blank"><i class="fa fa-instagram fa-2x"></i></a></div>')},set_chargeble_icons=function(){if(document.getElementsByClassName("listing-details-container").length)for(var t=document.getElementsByClassName("col-12"),e=0,i=t.length;e<i;++e)-1!=t[e].innerHTML.indexOf("<b>Chargeable services and facilities:</b>")&&t[e].setAttribute("id","chargeable")},tracking_codes=function(){var t=window.location.href;if(-1!==t.indexOf("confirmation_pending")||-1!==t.indexOf("community_memberships/pending_consent")){var e=document.createElement("script"),i=document.createElement("script"),o=document.createElement("noscript"),a=document.createElement("script");a.innerHTML="fbq('track', 'CompleteRegistration');",document.body.insertBefore(a,document.body.firstChild),i.type="text/javascript",i.setAttribute("src","//www.googleadservices.com/pagead/conversion.js"),e.type="text/javascript",e.innerHTML="/* <![CDATA[ */ var google_conversion_id = 848665749;var google_conversion_language = 'en';var google_conversion_format = '3';var google_conversion_color = 'ffffff';var google_conversion_label = 'qhk7CIjRrnQQlbnWlAM';var google_remarketing_only = false;/* ]]> */",document.body.appendChild(e),document.body.appendChild(i),o.innerHTML='<div style="display:inline;"><img height="1" width="1" style="border-style:none;" alt="" src="//www.googleadservices.com/pagead/conversion/848665749/?label=qhk7CIjRrnQQlbnWlAM&amp;guid=ON&amp;script=0"/></div>',document.body.appendChild(o)}},schema=function(){var t=document.getElementById("homepage-filters"),e=document.getElementsByClassName("origin"),i=document.getElementsByTagName("head")[0];if(null!==t)(r=document.createElement("script")).type="application/ld+json",r.text=JSON.stringify({"@context":"http://schema.org","@type":["WebSite","Organization"],url:"https://www.misterlocation.com/",potentialAction:{"@type":"SearchAction",target:"https://www.misterlocation.com/?q={search_term_string}","query-input":"required name=search_term_string"},name:"MisterLocation",alternateName:"Mister Location",logo:"https://user-assets.sharetribe.com/images/communities/wide_logos/36321/header_highres/MIsterlocationlogo_horiz.jpg",sameAs:["https://www.facebook.com/misterlocationOfficial","https://twitter.com/Mister_Location","https://www.instagram.com/misterlocation/"],description:"Find and book photo studios and locations",disambiguatingDescription:"Find and book photo studios and locations"}),i.appendChild(r);else if(null===t&&1===e.length){var o=document.getElementsByClassName("listing-price-amount")[0].innerHTML.substr(2),a=document.getElementById("listing-title"),n=document.getElementsByClassName("listing-image"),s=window.location.href.split("/listings/")[1].split("-");mpn=s[0];var r=document.createElement("script");if(r.type="application/ld+json",null!=n[0])var c=n[0].getAttribute("src").split("?")[0];r.text=JSON.stringify({"@context":"http://schema.org","@type":"Product",name:a.innerHTML,image:c,mpn:mpn,offers:{"@type":"Offer",priceCurrency:"EUR",price:o,businessFunction:"http://purl.org/goodrelations/v1#LeaseOut",seller:{"@type":"Organization",name:"MisterLocation",alternateName:"Mister Location",logo:"https://user-assets.sharetribe.com/images/communities/wide_logos/36321/header_highres/MIsterlocationlogo_horiz.jpg",url:"https://www.misterlocation.com",sameAs:["https://www.facebook.com/misterlocationOfficial","https://twitter.com/Mister_Location","https://www.instagram.com/misterlocation/"],description:"Find and book photo studios and locations",disambiguatingDescription:"Find and book photo studios and locations"}}}),i.appendChild(r)}},all_func=function(){set_social_icons(),set_chargeble_icons(),schema(),tracking_codes()};window.onload=all_func;
+function myFunction(id) {
+    var x = document.getElementById(id);
+    if (x.style.display === 'none') {
+        x.style.display = 'block';
+    } else {
+        x.style.display = 'none';
+    }
+}
+
+
+
+var set_social_icons = function () {
+
+    var branding_content = document.querySelectorAll(".MenuPriority>div:last-child");
+    
+
+    if (window.innerWidth >= 996) {
+
+      branding_content[0].insertAdjacentHTML('afterend', '<a href="https://www.facebook.com/misterlocationOfficial" target="blank"><i class="fa fa-facebook fa-2x"></i></a><a href="https://twitter.com/Mister_Location" target="blank"><i class="fa fa-twitter fa-2x"></i></a><a href="https://www.instagram.com/misterlocation/" target="blank"><i class="fa fa-instagram fa-2x"></i></a>');
+
+
+    }
+    else {
+      var mobile_menu = document.querySelectorAll(".MenuSection>div:last-child");
+
+mobile_menu[0].insertAdjacentHTML('afterend', '<div class="mobilesocial"><a href="https://www.facebook.com/misterlocationOfficial" target="blank"><i class="fa fa-facebook fa-2x"></i></a><a href="https://twitter.com/Mister_Location" target="blank"><i class="fa fa-twitter fa-2x"></i></a><a href="https://www.instagram.com/misterlocation/" target="blank"><i class="fa fa-instagram fa-2x"></i></a></div>');
+
+    }
+
+}
+
+
+var set_chargeble_icons = function () {
+
+
+    var listing_page = document.getElementsByClassName('listing-details-container');
+
+    if(listing_page.length){
+
+      var divs = document.getElementsByClassName('col-12');
+      for (var i = 0, len = divs.length; i < len; ++i) { 
+      if(divs[i].innerHTML.indexOf("<b>Chargeable services and facilities:</b>") != -1) {
+
+      divs[i].setAttribute('id', 'chargeable'); 
+      /*var columns = divs[i].querySelectorAll('.row > .col-4');
+      columns_length = columns.length;
+      for (var x = 0, len = columns_length; x < len; ++x) {
+        minirows = columns[x].querySelectorAll('.selected');
+        minirows_length = minirows.length;
+        for (var y = 0, len_m = minirows_length; y < len_m; ++y) {
+          minirows[y].querySelector('.checkbox-option-checkmark > i').setAttribute('class','fa fa-eur icon-fix icon-margin');
+        }
+      }  
+*/
+
+      }
+      }
+
+
+  }
+}
+
+
+
+/* test - 
+
+var sanitize_name = function () {
+
+  var name = document.getElementById("listing-author-link");
+  if (name !== null) {
+  if (name.innerHTML.indexOf(".") !== -1) {
+    string = name.innerHTML.split(".", 1);
+    name.innerHTML = string;
+  }
+
+}
+
+
+}
+
+
+
+
+
+
+var set_labels = function () {
+
+
+
+	if (window.innerWidth >= 768){
+
+var selection = document.querySelector("a[title=Grid]") !== null;
+
+if (selection) {
+
+	var grid_btn = document.querySelector("a[title=Grid]");
+	var grid_btn2 = document.querySelector("a[title=List]");
+
+
+}
+
+else {
+
+	var grid_btn = document.querySelector("a[title=Griglia]");
+	var grid_btn2 = document.querySelector("a[title=Lista]");
+}
+
+
+
+
+grid_btn.setAttribute("href", "/?category=studio");
+grid_btn.innerHTML = "STUDIO";
+grid_btn2.setAttribute("href", "/?category=location");
+grid_btn2.innerHTML = "LOCATION";
+
+var menu = document.querySelector(".visible-tablet > .row > .col-12");
+menu.classList.add("hide");
+
+var url = window.location.href;
+
+if ((url.indexOf("category=studio") === -1) && (url.indexOf("category=location") === -1)) {
+
+    grid_btn2.classList.remove("selected");
+	grid_btn.classList.remove("selected");
+
+}
+
+else if ( (url.indexOf("category=studio") !== -1) && (url.indexOf("category=location") === -1) ){
+
+	grid_btn.classList.add("selected");
+	grid_btn2.classList.remove("selected");
+}
+
+else if ( (url.indexOf("category=location") !== -1) && (url.indexOf("category=studio") === -1) )
+
+{
+
+	grid_btn2.classList.add("selected");
+	grid_btn.classList.remove("selected");
+}
+}
+};
+*/
+
+var tracking_codes = function () {
+
+  var url = window.location.href;
+  if ((url.indexOf("confirmation_pending") !== -1) || (url.indexOf("community_memberships/pending_consent") !== -1)) {
+   
+  var newElem = document.createElement( 'script'); //create a script tag
+  var google_script_link = document.createElement('script');
+  var noscript = document.createElement('noscript');
+  var fbscript = document.createElement('script');
+  fbscript.innerHTML = "fbq('track', 'CompleteRegistration');"
+  document.body.insertBefore(fbscript, document.body.firstChild);
+  google_script_link.type = 'text/javascript';
+  google_script_link.setAttribute('src','//www.googleadservices.com/pagead/conversion.js');
+  newElem.type = 'text/javascript'; // add type attribute
+  newElem.innerHTML = "/* <![CDATA[ */ var google_conversion_id = 848665749;var google_conversion_language = 'en';var google_conversion_format = '3';var google_conversion_color = 'ffffff';var google_conversion_label = 'qhk7CIjRrnQQlbnWlAM';var google_remarketing_only = false;/* ]]> */"; // add content i.e. function definition and a call 
+  document.body.appendChild(newElem);
+  document.body.appendChild(google_script_link);
+  noscript.innerHTML = '<div style="display:inline;"><img height="1" width="1" style="border-style:none;" alt="" src="//www.googleadservices.com/pagead/conversion/848665749/?label=qhk7CIjRrnQQlbnWlAM&amp;guid=ON&amp;script=0"/></div>';
+  document.body.appendChild(noscript);
+
+
+  }
+}
+
+
+var schema = function () {
+
+  var home = document.getElementById('homepage-filters');
+  var map = document.getElementsByClassName('origin');
+  var h = document.getElementsByTagName('head')[0];
+  /*var address = document.getElementById('origin_loc_address').value; */
+
+    if (home !== null) {
+
+
+      /*var titles = document.getElementsByClassName('fluid-thumbnail-grid-image-title');
+      var prices = document.getElementsByClassName('fluid-thumbnail-grid-image-price');
+      var images = document.getElementsByClassName('fluid-thumbnail-grid-image-image');*/
+      
+
+      //create JSON-LD for lastModified and citation_online_date
+      var el = document.createElement('script');  
+      el.type = 'application/ld+json';
+     /* var jsonArr = [];
+      for (i=0;i<titles.length;i++) {
+        if(images[i]!=null){
+        jsonArr.push(
+             { "@type": "LocalBusiness",
+              "priceRange": prices[i].innerHTML,
+              "image": images[i].getAttribute('src').split("?")[0],
+              "name": titles[i].innerHTML
+            }
+          );
+        } 
+
+      } */
+
+      el.text = JSON.stringify(
+
+            {
+              "@context": "http://schema.org",
+              "@type": ["WebSite", "Organization"],
+              "url": "https://www.misterlocation.com/",
+              "potentialAction": {
+              "@type": "SearchAction",
+              "target": "https://www.misterlocation.com/?q={search_term_string}",
+    "query-input": "required name=search_term_string"
+  },
+            "name": "MisterLocation",
+            "alternateName": "Mister Location",
+             "logo": "https://user-assets.sharetribe.com/images/communities/wide_logos/36321/header_highres/MIsterlocationlogo_horiz.jpg",
+            "sameAs" : ["https://www.facebook.com/misterlocationOfficial", "https://twitter.com/Mister_Location", "https://www.instagram.com/misterlocation/"],
+            "description": "Find and book photo studios and locations",
+            "disambiguatingDescription": "Find and book photo studios and locations"
+            }
+
+
+         );
+
+  
+
+      h.appendChild(el);
+
+    } else  if ((home === null)&&(map.length === 1)) {  
+
+     
+
+      var price = document.getElementsByClassName('listing-price-amount');
+      var price_raw = price[0].innerHTML.substr(2);
+      var name = document.getElementById('listing-title');
+      var images = document.getElementsByClassName('listing-image');
+      var url_splitted = window.location.href.split( "/listings/");
+      var url_last_part = url_splitted[1].split("-");
+      mpn = url_last_part[0];
+      /*var amenities = document.getElementsByClassName('selected');
+      var jsonArrAm = [];*/
+      /*
+      if(amenities[2]!=null){
+        for(i=2;i<amenities.length;i++){
+          var spans = amenities[i].getElementsByTagName('span');
+          jsonArrAm.push(
+            { "@type": "LocationFeatureSpecification",
+              "name": spans[1].innerHTML
+            }
+          );
+
+        }
+      } */
+
+      
+
+      var el = document.createElement('script');  
+      el.type = 'application/ld+json';  
+     
+
+       if(images[0]!=null){
+        var mainimage = images[0].getAttribute('src').split("?")[0];
+       /* var jsonArr = [{"@type": "ImageGallery", "primaryImageOfPage": mainimage}];
+        for (i=0;i<images.length;i++){
+        jsonArr.push(
+             { "@type": "ImageObject",
+              "image": images[i].getAttribute('src').split("?")[0]
+            }
+          );
+        } */
+
+      }
+
+
+      el.text = JSON.stringify({
+
+         "@context" : "http://schema.org",
+            "@type" : "Product",
+            "name": name.innerHTML,
+            "image": mainimage,
+            "mpn": mpn,
+            "offers": {
+                "@type": "Offer",
+                "priceCurrency": "EUR",
+                "price": price_raw,
+                "businessFunction": "http://purl.org/goodrelations/v1#LeaseOut",
+                "seller" : {
+                  "@type" : "Organization",
+                  "name": "MisterLocation",
+                  "alternateName": "Mister Location",
+                  "logo": "https://user-assets.sharetribe.com/images/communities/wide_logos/36321/header_highres/MIsterlocationlogo_horiz.jpg",
+                  "url" : "https://www.misterlocation.com",
+                  "sameAs" : ["https://www.facebook.com/misterlocationOfficial", "https://twitter.com/Mister_Location", "https://www.instagram.com/misterlocation/"],
+                  "description": "Find and book photo studios and locations",
+                  "disambiguatingDescription": "Find and book photo studios and locations",
+                }
+
+                                      
+            }
+
+      });
+      h.appendChild(el);
+
+    }
+}
+
+
+
+var all_func = function () {
+
+  
+  set_social_icons();
+  set_chargeble_icons();
+  schema();
+  tracking_codes();
+  /*sanitize_name();*/
+  /* set_labels(); */
+
+}
+
+/* window.onresize = set_labels; */
+window.onload = all_func;
