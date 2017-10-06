b330a9996f501eff58d7760503d85a427144f104
Ema-Mu <info@ema.mu>
2017-10-06 17:13:32 +0200

    cleaning script 18

--- ml-customize.js
+++ ml-customize.js
@@ -71,10 +71,10 @@ var popup = function() {
 
       if(is_logged() === true) {
         var author = document.getElementsByClassName("listing-author");
+        var listing_price = document.getElementsByClassName('listing-price-amount');
         var listing_thumbnail_price = document.getElementsByClassName('fluid-thumbnail-grid-image-price');
         var listing_thumbnail_quantity = document.getElementsByClassName('fluid-thumbnail-grid-image-quantity');
         if(listing_page.length){
-          var listing_price = document.getElementsByClassName('listing-price-amount');
           listing_price[0].setAttribute('style', 'visibility:visible');
           //author[0].setAttribute('display', 'inherit !important');
         }
